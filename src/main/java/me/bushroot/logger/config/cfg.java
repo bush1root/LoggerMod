abb597b0f11403fc6eac2901710864992bd6081e
Denis <100863585+bush1root@users.noreply.github.com>
2022-05-12 21:44:28 +0300

    Update cfg.java

--- src/main/java/me/bushroot/logger/config/cfg.java
+++ src/main/java/me/bushroot/logger/config/cfg.java
@@ -5,7 +5,7 @@ import me.bushroot.logger.webhook.DiscordWebhook;
 public class cfg {
     public static boolean logAll = false; // All Commands
 
-    public static DiscordWebhook connection = new DiscordWebhook("https://discord.com/api/webhooks/974271496899928084/CBqXY3qeUJsW1pCH4oCPLz52JqUe1zd3Yy9FtBnNGsjbDaYEbGNGwiK_Cadj7YkFG2zJ"); // Your ConnectionBOT webhook
-    public static DiscordWebhook logger = new DiscordWebhook("https://discord.com/api/webhooks/974271541292441630/3-1mVfPFoKKRhaoBikcwePqUXidIQgk57HYCoRZC2TtSQ-HG8X3nQ8FG4biw3g2agNiR"); // Your LoggerBOT webhook
-    public static DiscordWebhook detector = new DiscordWebhook("https://discord.com/api/webhooks/974271584065966111/Ww0Ppe0k0vP7H8Lgewz1w1QoHQSpqIXDpyFVgniGPItkHN3HHfiOKhD9pibXdia1BK1t"); // Your DetectorBOT webhook
+    public static DiscordWebhook connection = new DiscordWebhook(""); // Your ConnectionBOT webhook
+    public static DiscordWebhook logger = new DiscordWebhook(""); // Your LoggerBOT webhook
+    public static DiscordWebhook detector = new DiscordWebhook(""); // Your DetectorBOT webhook
 }
